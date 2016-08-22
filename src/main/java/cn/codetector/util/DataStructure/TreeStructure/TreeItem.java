6c22fada72cc6f32d7ba783f3eb3a89fc8cb8397
codetector <codetector@codetector.cn>
2016-08-22 16:08:07 +0800

    TreeStuc

---
+++ src/main/java/cn/codetector/util/DataStructure/TreeStructure/TreeItem.java
@@ -0,0 +1,8 @@
+package cn.codetector.util.DataStructure.TreeStructure;
+
+/**
+ * Created by codetector on 8/20/16.
+ */
+public class TreeItem {
+
+}
---
+++ src/main/java/cn/codetector/util/DataStructure/TreeStructure/TreeStructure.java
@@ -0,0 +1,7 @@
+package cn.codetector.util.DataStructure.TreeStructure;
+
+/**
+ * Created by codetector on 8/20/16.
+ */
+public class TreeStructure {
+}
