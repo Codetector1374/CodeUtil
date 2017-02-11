ffa3f31d93fdb2b8bcffc8030999c65ea8824afa
codetector <codetector@codetector.cn>
2017-02-11 14:14:32 -0500

    Making up UnitTests

---
+++ src/test/java/cn/codetector/util/Graphics/Vector/Vec2Test.java
@@ -0,0 +1,79 @@
+package cn.codetector.util.Graphics.Vector;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by codetector on 11/02/2017.
+ */
+public class Vec2Test {
+    Vec2 vec2 = new Vec2(1,2);
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getX() throws Exception {
+
+    }
+
+    @Test
+    public void getY() throws Exception {
+
+    }
+
+    @Test
+    public void getWidthi() throws Exception {
+
+    }
+
+    @Test
+    public void getHeighti() throws Exception {
+
+    }
+
+    @Test
+    public void getWidthf() throws Exception {
+
+    }
+
+    @Test
+    public void getHeightf() throws Exception {
+
+    }
+
+    @Test
+    public void setX() throws Exception {
+
+    }
+
+    @Test
+    public void setY() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void multiply() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+}
